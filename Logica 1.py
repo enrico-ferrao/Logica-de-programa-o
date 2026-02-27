bb3a96c7bb53bb1174dd5f2be7871b3fbf3cb02f
enrico-ferrao <enricogferrao@gmail.com>
2026-02-27 01:32:39 -0300

    Create Logica 1.py
    Aula do dia 13/02

---
+++ Logica 1.py
@@ -0,0 +1,69 @@
+print ("Hello,  World")
+
+a = 2
+b = 2
+soma = a + b
+print (f"soma = {soma}")
+
+a = float (2)
+b = float (2)
+soma = a + b
+print (f"soma = {soma}")
+
+numero_inteiro = float (10)
+numero_decimal = float (0.75)
+conta = numero_inteiro * numero_decimal
+print (f"conta = {conta}")
+
+a = 10
+b = 3
+c = 2.75
+print (f"a + b = {a + b}")
+print (f"a - c = {a - c}")
+print (f"b x c = {b * c}")
+# + soma
+# - subtração
+# / divisão
+# // divisão inteira
+# % modulo (resto da divisão inteira)
+
+print (f"a / b = {a / b}")
+print (f"a // b = {a // b}")
+
+print (f"a / c = {a / c}")
+print (f"a // c = {a // c}")
+
+print (f"a % b = {a % b}")
+print (f"a % c = {a % c}")
+
+e_verdade = True
+print (f"é verdade? {e_verdade}")
+
+nome = "Enrico"
+sobrenome = "Ferrão"
+nome_completo = nome + " " + sobrenome
+print (nome_completo)
+
+a = 54
+b = 12
+print ("a = %d" %a)
+print ("%d + %d = %d" %(a, b, a+b))
+
+# %s = strings
+# %d = inteiros
+# %f = float
+# %b = boolean
+
+palavra = "premio"
+valor = 1000
+print ("%s = %d" %(palavra, valor))
+
+print ("Este é\num exemplo de\nquebra de linhas")
+
+nome = input("digita seu nome: ")
+cor = input(f"{nome} Qual sua cor favorita? ")
+print (f"Nossa, {nome} minha cor favorita também é {cor}")
+
+nome1 = input("Qual seu nome? ")
+sobrenome1 = input("Qual seu sobrenome? ")
+print (f"{nome1} {sobrenome1}")
