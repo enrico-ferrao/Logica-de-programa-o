da0b4f954347daa03d1ff3be463aed7281a60b9d
enrico-ferrao <enricogferrao@gmail.com>
2026-03-05 19:51:04 -0300

    Create sistemas_compras.py

---
+++ sistemas_compras.py
@@ -0,0 +1,42 @@
+#valor de compras.
+# input forma de pagamento.
+# avista sendo variavel 1 ( pix c/10% de desconto).
+# variavel 2 (boleto c/5% de desconto).
+# essas 2 variaveis sao em if dentro do primero if
+# aprazo + numero de parcelas (2 vezes e o valor final), (3 vezes + 5% de acrescimo + valor final), (+ que 3 é invalido)
+# caso nao seja avista ou aprazo TEM QUE DAR INVALIDO
+
+
+valor_compra = float(input("Qual valor da compra? ").replace(",", "."))
+forma_de_pagamento = int(input("Qual forma de pagamento ? (avista = 1, aprazo = 2) "))
+
+
+if forma_de_pagamento == 1:
+    avista = int(input("Avista no pix ou no boleto? (pix = 1, boleto = 2) "))
+    if avista == 1:
+        desconto = valor_compra * 0.1
+        valor_final = valor_compra - desconto
+        print (f"Sua compra deu: R${valor_final} e seu desconto foi de R${desconto} ")
+    else:
+        desconto = valor_compra * 0.05
+        valor_final = valor_compra - desconto
+        print ("Sua compra deu: R$ %.2d e seu desconto foi de R$ %.2d "  %(valor_final,desconto))
+
+
+elif forma_de_pagamento == 2:
+    aprazo = int(input("Aprazo, em quantas parcelas: (2 vez = 2, 3 vezes = 3) "))
+    if aprazo == 2:
+        print (f"Sua compra deu: R${valor_compra} e a compra foi divida em: {aprazo} vezes s/ juros ")
+    
+    elif aprazo == 3:
+        acrescimo = valor_compra * 0.05
+        valor_final = valor_compra + acrescimo
+        print (f"Sua compra deu: R${valor_final} e a compra foi divida em: {aprazo} vezes com 5% juros ")
+
+    else:
+        print ("Mais do que 3 vezes, não é permitido! ")
+
+else:
+    print("Opção invalida!")
+
+
